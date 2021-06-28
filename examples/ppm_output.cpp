816e02e7d29c71d95fe351f23c1e85010b5648cb
Kazu Otani <kazuotani14@gmail.com>
2021-06-27 21:17:08 -1000

    Final render, low res

--- examples/ppm_output.cpp
+++ examples/ppm_output.cpp
@@ -18,6 +18,61 @@
 
 #include <iostream>
 
+HittableList random_scene()
+{
+  HittableList world;
+
+  auto ground_material = make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
+  world.add(make_shared<Sphere>(Point3(0, -1000, 0), 1000, ground_material));
+
+  for (int a = -11; a < 11; ++a)
+  {
+    for (int b = -11; b < 11; ++b)
+    {
+      Point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+      if ((center - Point3(4, 0.2, 0)).length() > 0.9)
+      {
+        shared_ptr<Material> sphere_material;
+
+        auto choose_mat = random_double();
+        if (choose_mat < 0.8)
+        {
+          // diffuse
+          auto albedo = Vec3::random() * Vec3::random();
+          sphere_material = make_shared<Lambertian>(albedo);
+          world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+        }
+        else if (choose_mat < 0.95)
+        {
+          // metal
+          auto albedo = Vec3::random(0.5, 1);
+          auto fuzz = random_double(0, 0.5);
+          sphere_material = make_shared<Metal>(albedo, fuzz);
+          world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+        }
+        else
+        {
+          // glass
+          sphere_material = make_shared<Dielectric>(1.5);
+          world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+        }
+      }
+    }
+  }
+
+  auto material1 = make_shared<Dielectric>(1.5);
+  world.add(make_shared<Sphere>(Point3(0, 1, 0), 1.0, material1));
+
+  auto material2 = make_shared<Lambertian>(Color(0.4, 0.2, 0.1));
+  world.add(make_shared<Sphere>(Point3(-4, 1, 0), 1.0, material2));
+
+  auto material3 = make_shared<Metal>(Color(0.7, 0.6, 0.5), 0.0);
+  world.add(make_shared<Sphere>(Point3(4, 1, 0), 1.0, material3));
+
+  return world;
+}
+
 Color ray_color(const Ray &r, const Hittable &world, int depth)
 {
   // If we've exceeded the ray bounce limit, no more light is gathered.
@@ -45,31 +100,21 @@ Color ray_color(const Ray &r, const Hittable &world, int depth)
 int main()
 {
   // Image
-  const double aspect_ratio = 16.0 / 9.0;
-  const int image_width = 400;
+  const auto aspect_ratio = 3.0 / 2.0;
+  const int image_width = 240;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
   const int samples_per_pixel = 100;
   const int max_depth = 50;
 
   // World
-  HittableList world;
-  auto material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
-  auto material_center = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-  auto material_left = make_shared<Dielectric>(1.5);
-  auto material_right = make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.0);
-
-  world.add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, material_ground));
-  world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, material_center));
-  world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
-  world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), -0.45, material_left));
-  world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right));
+  auto world = random_scene();
 
   // Camera
-  Point3 lookfrom(3, 3, 2);
-  Point3 lookat(0, 0, -1);
+  Point3 lookfrom(13, 2, 3);
+  Point3 lookat(0, 0, 0);
   Vec3 vup(0, 1, 0);
-  double dist_to_focus = (lookfrom - lookat).length();
-  double aperture = 2.0;
+  double dist_to_focus = 10.0;
+  double aperture = 0.1;
 
   Camera cam(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus);
 
--- src/color.h
+++ src/color.h
@@ -4,7 +4,7 @@
 
 #include <iostream>
 
-void write_color(std::ostream &out, const Color &pixel_color)
+inline void write_color(std::ostream &out, const Color &pixel_color)
 {
   auto r = pixel_color.x();
   auto g = pixel_color.y();
