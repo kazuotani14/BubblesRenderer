6480f9ec38fdc3ff7d26af3981630173e3f2591f
Kazu Otani <kazuotani14@gmail.com>
2021-07-12 10:24:00 -0700

    Revert "Initial attempt at parallelization: parallel pixel sampling.  no speedup"
    This reverts commit a5938c362bcbcc20da98f5661cb62a0a66fa523a.

--- src/render.h
+++ src/render.h
@@ -8,8 +8,6 @@
 #include "timing.h"
 
 #include <iostream>
-#include <mutex>
-#include <thread>
 
 Color ray_color(const Ray &r, const Color &background, const Hittable &world, int depth)
 {
@@ -38,30 +36,10 @@ Color ray_color(const Ray &r, const Color &background, const Hittable &world, in
   return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
 }
 
-void render(std::ostream &out,
-            const Hittable &world,
-            const Camera &cam,
-            int H,
-            int W,
-            const Color &background,
-            int samples_per_pixel,
-            int max_depth,
-            int num_threads = 1,
-            bool print_progress = true)
+void render(std::ostream &out, const Hittable &world, const Camera &cam, int H, int W, const Color &background, int samples_per_pixel, int max_depth, bool print_progress = true)
 {
-  static bool printed = false;
-  if (!printed)
-  {
-    std::cerr << "hardware conc: " << std::thread::hardware_concurrency() << ", " << num_threads << std::endl;
-    printed = true;
-  }
-
   out << "P3\n"
       << W << ' ' << H << "\n255\n";
-
-  std::vector<std::thread> threads(num_threads);
-  std::mutex color_mutex;
-  int samples_per_thread = samples_per_pixel / num_threads;
 
   // pixel values are listed in row-major order
   for (int row = H - 1; row >= 0; --row) // scan from top row down (for ppm format)
@@ -72,30 +50,14 @@ void render(std::ostream &out,
     for (int col = 0; col < W; ++col)
     {
       Color pixel_color(0, 0, 0);
-
-      auto sample_color = [&](int num_samples)
+      for (int s = 0; s < samples_per_pixel; ++s)
       {
-        for (int i = 0; i < num_samples; ++i)
-        {
-          auto u = (col + random_double()) / (W - 1);
-          auto v = (row + random_double()) / (H - 1);
-          Ray r = cam.get_ray(u, v);
-
-          std::lock_guard<std::mutex> lock(color_mutex);
-          pixel_color += ray_color(r, background, world, max_depth);
-        }
-      };
-
-      for (int t_idx = 0; t_idx < num_threads; ++t_idx)
-      {
-        int extra_samples = t_idx == 0 ? samples_per_pixel % num_threads : 0;
-        threads[t_idx] = std::thread(sample_color, samples_per_thread + extra_samples);
+        auto u = (col + random_double()) / (W - 1);
+        auto v = (row + random_double()) / (H - 1);
+        Ray r = cam.get_ray(u, v);
+        pixel_color += ray_color(r, background, world, max_depth);
       }
-
-      for (int t_idx = 0; t_idx < num_threads; ++t_idx)
-        threads[t_idx].join();
-
-      pixel_color = pixel_color / samples_per_pixel;
+      pixel_color /= samples_per_pixel;
 
       write_color(out, pixel_color);
     }
