02f10c055d852cee9c6c0c506662eebfb9032a6e
Kazu Otani <kazuotani14@gmail.com>
2021-07-12 10:46:59 -0700

    Enable parallel rendering: parallelize row blocks

--- TODOs.md
+++ TODOs.md
@@ -1,22 +1,25 @@
 # TODOs
 
-* Ray tracing: the next week
-  * go through section 5: perlin noise
-  * section 9 (volumes) -- understand the ConstantMedium::hit math
-  * re-visit 3.4: correctly handle zeros and nans
-
 * for cornell box: how to reduce noise? see "rest of your life" for importance sampling
 
-* clean up code
-  * improve timer utility: make it scoped and record to static data structure. Or just take from ethz's messer code
-  * Optimize code
-    * use multiple threads. What's the best way to do this?
-    * try optimizing: look for hotstops with callgrind?
+* speed up bottlenecks
+  * rendering: hit
+      * parallelize samples or pixels: threads, GPU
+      * profile and optimize individual primitives' hit methods
+  * fluids: finding neighbors
+    * spatial hash
+    * At current scale, rendering takes much more time
+
+* Ray tracing: the next week
+  * section 9 (volumes) -- derive the ConstantMedium::hit math
+  * re-visit 3.4: correctly handle zeros and nans
+  * go through section 5: perlin noise
+
+* clean up
   * Add clang-format file
     * references should be attached to type
     * support c++17 (i.e. `std::vector<std::shared_ptr<Hittable> >` shouldn't have a space)
   * add unit tests?
-  * turn on compiler warnings
 
 * future projects
   * beach sim: static sand (image texture) to start, then dynamic
--- src/render.h
+++ src/render.h
@@ -8,6 +8,8 @@
 #include "timing.h"
 
 #include <iostream>
+#include <mutex>
+#include <thread>
 
 Color ray_color(const Ray &r, const Color &background, const Hittable &world, int depth)
 {
@@ -36,36 +38,61 @@ Color ray_color(const Ray &r, const Color &background, const Hittable &world, in
   return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
 }
 
-void render(std::ostream &out, const Hittable &world, const Camera &cam, int H, int W, const Color &background, int samples_per_pixel, int max_depth, bool print_progress = true)
+void render(std::ostream &out, const Hittable &world, const Camera &cam, int H, int W, const Color &background, int samples_per_pixel, int max_depth, int num_threads = std::thread::hardware_concurrency(), bool print_progress = true)
 {
-  out << "P3\n"
-      << W << ' ' << H << "\n255\n";
-
   std::vector<Color> pixel_values(H * W);
+  int num_pixels_done = 0;
+  std::mutex color_mutex;
 
-  // pixel values are listed in row-major order
-  for (int row = H - 1; row >= 0; --row) // scan from top row down (for ppm format)
+  auto render_rows = [&](int r0, int r1) // inclusive range
   {
-    if (print_progress)
-      std::cerr << "\rScanlines remaining: " << row << ' ' << std::flush;
-
-    for (int col = 0; col < W; ++col)
+    for (int row = r0; row <= r1; ++row)
     {
-      Color pixel_color(0, 0, 0);
-      for (int s = 0; s < samples_per_pixel; ++s)
+      for (int col = 0; col < W; ++col)
       {
-        auto u = (col + random_double()) / (W - 1);
-        auto v = (row + random_double()) / (H - 1);
-        Ray r = cam.get_ray(u, v);
-        pixel_color += ray_color(r, background, world, max_depth);
-      }
-      pixel_color /= samples_per_pixel;
+        Color pixel_color(0, 0, 0);
+        for (int s = 0; s < samples_per_pixel; ++s)
+        {
+          auto u = (col + random_double()) / (W - 1);
+          auto v = (row + random_double()) / (H - 1);
+          Ray r = cam.get_ray(u, v);
+          pixel_color += ray_color(r, background, world, max_depth);
+        }
+        pixel_color /= samples_per_pixel;
 
-      int c_idx = col + (H - 1 - row) * W;
-      pixel_values[c_idx] = pixel_color;
+        // pixel values are listed in row-major order in ppm format, from top down
+        int c_idx = col + (H - 1 - row) * W;
+
+        std::lock_guard<std::mutex> lock(color_mutex);
+        pixel_values[c_idx] = pixel_color;
+        if (print_progress)
+          std::cerr << "\rPixels done: " << int(double(++num_pixels_done) / (H * W) * 100) << "% " << std::flush;
+      }
     }
+  };
+
+  if (num_threads == 1)
+  {
+    render_rows(0, H - 1);
+  }
+  else
+  {
+    std::vector<std::thread> threads(num_threads);
+    int rows_per_thread = H / num_threads;
+
+    for (int t_idx = 0; t_idx < num_threads; ++t_idx)
+    {
+      int start_row = t_idx * rows_per_thread;
+      int end_row = t_idx == num_threads - 1 ? H - 1 : start_row + rows_per_thread - 1;
+      threads[t_idx] = std::thread(render_rows, start_row, end_row);
+    }
+
+    for (int t_idx = 0; t_idx < num_threads; ++t_idx)
+      threads[t_idx].join();
   }
 
+  out << "P3\n"
+      << W << ' ' << H << "\n255\n";
   for (const auto &c : pixel_values)
     write_color(out, c);
 
