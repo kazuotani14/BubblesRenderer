afdb8d30c88c8a09618f29d78ecb60cf26a250b5
Kazu Otani <kazuotani14@gmail.com>
2021-07-15 14:49:45 -0700

    Add importance sampling for box

--- src/box.h
+++ src/box.h
@@ -19,10 +19,14 @@ public:
     return true;
   }
 
+  virtual double pdf_value(const Point3 &origin, const Vec3 &v) const override;
+  virtual Vec3 random(const Point3 &origin) const override;
+
 public:
   Point3 box_min;
   Point3 box_max;
-  HittableList sides;
+  std::vector<shared_ptr<Hittable> > sides;
+  HittableList sides_hittable;
 };
 
 Box::Box(const Point3 &p0, const Point3 &p1, shared_ptr<Material> ptr)
@@ -33,17 +37,33 @@ Box::Box(const Point3 &p0, const Point3 &p1, shared_ptr<Material> ptr)
   box_min = p0;
   box_max = p1;
 
-  sides.add(make_shared<XYRect>(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), ptr));
-  sides.add(make_shared<XYRect>(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr));
+  sides.push_back(make_shared<XYRect>(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), ptr));
+  sides.push_back(make_shared<XYRect>(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr));
 
-  sides.add(make_shared<XZRect>(p0.x(), p1.x(), p0.z(), p1.z(), p0.y(), ptr));
-  sides.add(make_shared<XZRect>(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), ptr));
+  sides.push_back(make_shared<XZRect>(p0.x(), p1.x(), p0.z(), p1.z(), p0.y(), ptr));
+  sides.push_back(make_shared<XZRect>(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), ptr));
 
-  sides.add(make_shared<YZRect>(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), ptr));
-  sides.add(make_shared<YZRect>(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr));
+  sides.push_back(make_shared<YZRect>(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), ptr));
+  sides.push_back(make_shared<YZRect>(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr));
+
+  for (const auto &s : sides)
+    sides_hittable.add(s);
 }
 
 bool Box::hit(const Ray &r, double t_min, double t_max, hit_record *rec) const
 {
-  return sides.hit(r, t_min, t_max, rec);
+  return sides_hittable.hit(r, t_min, t_max, rec);
+}
+
+double Box::pdf_value(const Point3 &origin, const Vec3 &v) const
+{
+  double pdf_val = 0;
+  for (const auto &s : sides)
+    pdf_val += s->pdf_value(origin, v);
+  return pdf_val / sides.size();
+}
+
+Vec3 Box::random(const Point3 &origin) const
+{
+  return sides[random_int(0, 5)]->random(origin);
 }
