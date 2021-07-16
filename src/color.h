056a122c41f1ba62ff10902a5054f9f0d3271998
Kazu Otani <kazuotani14@gmail.com>
2021-07-16 09:27:17 -0700

    revert hacks

--- src/color.h
+++ src/color.h
@@ -10,15 +10,6 @@ inline void write_color(std::ostream &out, const Color &pixel_color)
   auto g = pixel_color.y();
   auto b = pixel_color.z();
 
-  // // Replace NaN components with zero. Workaround for issues within renderer; use as needed
-  // // See detailed explanation in Ray Tracing: The Rest of Your Life.
-  // if (isnan(r))
-  //   r = 0.0;
-  // if (isnan(g))
-  //   g = 0.0;
-  // if (isnan(b))
-  //   b = 0.0;
-
   // Gamma-correct for gamma=2.0.
   r = sqrt(r);
   g = sqrt(g);
--- src/render.h
+++ src/render.h
@@ -64,31 +64,15 @@ void render(std::ostream &out, const Hittable &world, shared_ptr<Hittable> light
       for (int col = 0; col < W; ++col)
       {
         Color pixel_color(0, 0, 0);
-        int num_rejected = 0;
         for (int s = 0; s < samples_per_pixel; ++s)
         {
           auto u = (col + random_double()) / (W - 1);
           auto v = (row + random_double()) / (H - 1);
           Ray r = cam.get_ray(u, v);
 
-          // TODO
-          Color sample_color = ray_color(r, background, world, lights, max_depth);
-
-          // Hack to try to get rid of speckles
-          static constexpr double max_light = 15;
-          const bool nan_color = isnan(sample_color[0]) || isnan(sample_color[1]) || isnan(sample_color[2]);
-          const bool high_val_color = sample_color[0] > max_light || sample_color[1] > max_light || sample_color[2] > max_light;
-          if (nan_color || high_val_color)
-          {
-            ++num_rejected;
-            continue;
-          }
-
-          pixel_color += sample_color;
-
-          // pixel_color += ray_color(r, background, world, lights, max_depth);
+          pixel_color += ray_color(r, background, world, lights, max_depth);
         }
-        pixel_color /= (samples_per_pixel - num_rejected);
+        pixel_color /= samples_per_pixel;
 
         // pixel values are listed in row-major order in ppm format, from top down
         int c_idx = col + (H - 1 - row) * W;
